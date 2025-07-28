08aa4ea325b3b914d2a0dff92a68319c1eac2c57
daviramos1318 <ramosdavi241@gmail.com>
2025-07-28 10:09:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-// transformar menu hamburguer em X
-const menuHambueger = document.querySelector('.menu-hamburger');
+// Transformar menu hamburguer em x Add commentMore actions
+// Seleciona o ícone do menu hamburguer
+const menuHamburguer = document.querySelector('.menu-hamburguer');
 
-menuHambueger.addEventListener('click', () => {
+// Adiciona o evento de clique
+menuHamburguer.addEventListener('click', () => {
     toggleMenu();
 });
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+
+    // Alterna a classe "change" no ícone
+    menuHamburguer.classList.toggle('change');
+
+    // Mostra ou esconde o menu com base na classe "change"
+    if (menuHamburguer.classList.contains('change')) {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+}
